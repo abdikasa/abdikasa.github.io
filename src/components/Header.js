d42cd768a6c8faf872fe960c47d8634dc47f6525
abdikasa <abdikasa@sheridancollege.ca>
2020-12-27 21:10:13 -0500

    Added header component and MovieList component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import GoogleAuth from "./GoogleAuth";
+import Link from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="ui secondary  menu">
+      <a className="item">Shoppies</a>
+      <div className="right menu">
+        <GoogleAuth></GoogleAuth>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
