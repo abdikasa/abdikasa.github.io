d6605a3fd462cd48acf3513cf4d25f5e907a0639
abdikasa <abdikasa@sheridancollege.ca>
2020-12-27 21:08:30 -0500

    Added a bunch of dependencies.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, createStore, compose } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <Provider store={store}>
     <App></App>
