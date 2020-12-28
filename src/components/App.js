abd09b8bab9f9755eca176ac1ae847d72fcda80f
abdikasa <abdikasa@sheridancollege.ca>
2020-12-27 21:07:45 -0500

    Added Routes and created a history object to update the URL.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Router } from "react-router-dom";
+import MovieList from "./CRUD/MovieList";
+import history from "../history";
+import Header from "./Header";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="ui container">
+      <Header></Header>
+      <Router history={history}>
+        <Route path="/movies" component={MovieList} exact></Route>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
