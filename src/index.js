240e1da8f7cbf1641de25d0abc13a7fee225ad1f
abdikasa <abdikasa@sheridancollege.ca>
2020-12-28 20:13:43 -0500

    Added TypedJs.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./css/global.css";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore, compose } from "redux";
