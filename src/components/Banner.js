c3f8b57758bdd6c10b76e5cb72b2ab668ec2efbd
abdikasa <abdikasa@sheridancollege.ca>
2021-01-04 20:22:48 -0500

    Added banner when user reaches 5 movie nominations

---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Banner extends React.Component {
+  render() {
+    let marginTop = { marginTop: "2rem" };
+
+    if (this.props.totalNoms.length < 5) {
+      return null;
+    }
+    return (
+      <div className="ui center aligned grid" style={marginTop}>
+        <div
+          className="ui column banner test ad"
+          data-text="You have reached the maximum number of nominations (5)."
+          style={{ backgroundColor: "#252a4d" }}
+        ></div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { totalNoms: Object.values(state.nominatedFilm) };
+};
+
+export default connect(mapStateToProps)(Banner);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
 import GoogleAuth from "./GoogleAuth";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
+import Banner from "./Banner";
 
 const Header = () => {
   return (
-    <div className="ui secondary menu">
-      <a className="item">Shoppies</a>
-      <div className="right menu">
-        <GoogleAuth></GoogleAuth>
+    <div className="ui container">
+      <Banner></Banner>
+      <div className="ui secondary menu">
+        <Link to="/movies" className="item">
+          Shoppies
+        </Link>
+        <div className="right menu">
+          <GoogleAuth></GoogleAuth>
+        </div>
       </div>
     </div>
   );
